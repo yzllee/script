23fb351953de53dd3387e499ce014c12a55314b1
杨阿郑 <iamyazlee@hotmail.com>
2026-09-14 11:15:07 +0800

    Update moze.js

--- moze.js
+++ moze.js
@@ -4,7 +4,6 @@ MOZE解锁
 
 [rewrite_local]
 ^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/yzllee/script/refs/heads/script/moze.js
-^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/yzllee/script/refs/heads/script/moze.js
 
 [mitm]
 hostname = api.revenuecat.com, api.rc-backup.com
@@ -14,9 +13,7 @@ hostname = api.revenuecat.com, api.rc-backup.com
 const headers = $request.headers, ua = headers['User-Agent'] || headers['user-agent'], bundle_id = headers['X-Client-Bundle-ID'] || headers['x-client-bundle-id'];
 let customHB = {};
 let mozeBody = JSON.parse(typeof $response != "undefined" && $response.body || "{}");
-if (typeof $response == "undefined") {
-  console.log(bundle_id === "app.moze", ua.includes("MOZE"))
-} else if(mozeBody && mozeBody.subscriber) {
+if (typeof $response !== "undefined") {
   console.log(bundle_id === "app.moze", ua.includes("MOZE"))
   mozeBody.subscriber.entitlements["MOZE_PREMIUM_SUBSCRIPTION"] = {
     "expires_date": "6666-06-06T06:06:06Z",
