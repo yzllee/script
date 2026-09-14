1debf5f7e9c146d6daebea0eabbbb8860fb0070c
杨阿郑 <iamyazlee@hotmail.com>
2026-09-14 13:22:18 +0800

    Update moze.js

--- moze.js
+++ moze.js
@@ -4,29 +4,58 @@ MOZE解锁
 
 [rewrite_local]
 ^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/yzllee/script/refs/heads/script/moze.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/yzllee/script/refs/heads/script/moze.js
 
 [mitm]
 hostname = api.revenuecat.com, api.rc-backup.com
 
 */
 
-const headers = $request.headers, ua = headers['User-Agent'] || headers['user-agent'], bundle_id = headers['X-Client-Bundle-ID'] || headers['x-client-bundle-id'];
-let customHB = {};
-let mozeBody = JSON.parse(typeof $response != "undefined" && $response.body || "{}");
-if (typeof $response !== "undefined") {
-  console.log(bundle_id === "app.moze", ua.includes("MOZE"))
-  mozeBody.subscriber.entitlements["MOZE_PREMIUM_SUBSCRIPTION"] = {
-    "expires_date": "6666-06-06T06:06:06Z",
-    "purchase_date": "2023-08-16T03:56:24Z",
-    "product_identifier": "MOZE_PRO_SUBSCRIPTION_MONTHLY_BASIC"
-  };
-  mozeBody.subscriber.subscriptions["MOZE_PRO_SUBSCRIPTION_MONTHLY_BASIC"] = {
-    "expires_date": "6666-06-06T06:06:06Z",
-    "original_purchase_date": "2019-07-10T12:22:41Z",
-    "purchase_date": "2023-08-16T03:56:24Z",
-    "ownership_type" : "PURCHASED",
-    "store" : "app_store"
-  };
-  customHB.body = JSON.stringify(mozeBody);
+const requestHeaders = ($request && $request.headers) || {};
+const ua = requestHeaders["User-Agent"] || requestHeaders["user-agent"] || "";
+const bundleId =
+  requestHeaders["X-Client-Bundle-ID"] ||
+  requestHeaders["x-client-bundle-id"] ||
+  "";
+
+let result = {};
+
+if (bundleId !== "app.moze" || !/MOZE/i.test(ua)) {
+  $done({});
 }
-$done(customHB);
+
+if (typeof $response === "undefined") {
+  result.headers = requestHeaders;
+} else {
+  try {
+    const body = JSON.parse($response.body || "{}");
+
+    if (body.subscriber) {
+      body.subscriber.entitlements =
+        body.subscriber.entitlements || {};
+
+      body.subscriber.subscriptions =
+        body.subscriber.subscriptions || {};
+
+      body.subscriber.entitlements["MOZE_PREMIUM_SUBSCRIPTION"] = {
+        expires_date: "6666-06-06T06:06:06Z",
+        purchase_date: "2023-08-16T03:56:24Z",
+        product_identifier: "MOZE_PRO_SUBSCRIPTION_MONTHLY_BASIC"
+      };
+
+      body.subscriber.subscriptions["MOZE_PRO_SUBSCRIPTION_MONTHLY_BASIC"] = {
+        expires_date: "6666-06-06T06:06:06Z",
+        original_purchase_date: "2019-07-10T12:22:41Z",
+        purchase_date: "2023-08-16T03:56:24Z",
+        ownership_type: "PURCHASED",
+        store: "app_store"
+      };
+
+      result.body = JSON.stringify(body);
+    }
+  } catch (error) {
+    console.log("MOZE JSON parse error: " + error);
+  }
+}
+
+$done(result);
