5a29a6b7247e90fb04ae476e2773db7f92d8d896
杨阿郑 <iamyazlee@hotmail.com>
2026-09-14 09:57:21 +0800

    Update moze.js

--- moze.js
+++ moze.js
@@ -13,7 +13,7 @@ hostname = api.revenuecat.com, api.rc-backup.com
 
 const headers = $request.headers, ua = headers['User-Agent'] || headers['user-agent'], bundle_id = headers['X-Client-Bundle-ID'] || headers['x-client-bundle-id'];
 let moze = JSON.parse($response.body);
-if(bundle_id === "app.moze" || ua?.includes("MOZE")){
+if(bundle_id === "app.moze" || ua.includes("MOZE")){
   moze.subscriber.entitlements["MOZE_PREMIUM_SUBSCRIPTION"] = {
     "expires_date": "6666-06-06T06:06:06Z",
     "purchase_date": "2023-08-16T03:56:24Z",
